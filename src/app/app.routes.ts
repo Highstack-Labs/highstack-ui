a579a533ad5550693003dcec01814a1cf52e5587
diegoDevCodee <diegodevcodee@gmail.com>
2026-04-19 17:15:28 -0500

    feat: register atoms/button route with redirect from /

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ButtonPage } from './pages/atoms/button/button.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',             redirectTo: 'atoms/button', pathMatch: 'full' },
+  { path: 'atoms/button', component: ButtonPage },
+];
